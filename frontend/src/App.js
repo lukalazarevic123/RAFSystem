6e559032bfd6b4f46028e493729d06591ff9b497
lukalazarevic123 <lazarevicluka99@gmail.com>
2022-04-29 10:49:40 +0200

    dodata forma za kreiranje ispita

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
-import { Container } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 import Header from './components/Header';
 import ExamTable from './components/ExamTable';
+import NewExamForm from './components/NewExamForm';
 
 function App() {
   return (
@@ -9,6 +10,14 @@ function App() {
       <Header />
       <Container  className="justify-content-center text-center mt-5">
         <ExamTable exams = {[{id: 123, name: "asp", professor: "urosevic"}]}/>
+        <Row>
+          <Col>
+            <NewExamForm  exams = {[{id: 123, name: "asp", professor: "urosevic"}]}/>
+          </Col>
+          <Col>
+            <NewExamForm  exams = {[{id: 123, name: "asp", professor: "urosevic"}]}/>
+          </Col>
+        </Row>
       </Container>
       
     </div>
--- frontend/src/components/NewExamForm.js
+++ frontend/src/components/NewExamForm.js
@@ -1,8 +1,42 @@
 import { useState } from "react";
-import { Form } from "react-bootstrap";
+import { Button, Form, FormSelect, Spinner } from "react-bootstrap";
 
 export default function NewExamForm(props){
 
     const [examDate, setExamDate] = useState(0);
-    const [examId, setExamId] = useState(-1);
+    const [examID, setExamID] = useState(-1);
+    const [isLoading, setLoading] = useState(false);
+
+    const handleExam = async(evt) => {
+        evt.preventDefault();
+        setLoading(true);
+    }
+
+    return(
+        <Form onSubmit = {evt => handleExam(evt)}>
+            <h3>Create an exam</h3>
+            <Form.Group>
+                <Form.Label>Subject name</Form.Label>
+                <FormSelect onChange={evt => {setExamID(evt.target.value)}} required>
+                    <option value = "">Select a subject</option>
+                    {props.exams.map(exam => 
+                        <option key = {exam.id} value = {exam.id}>{exam.name}</option>    
+                    )}
+                </FormSelect>
+            </Form.Group>
+            <Form.Group>
+                <Form.Label>Exam date</Form.Label>
+                <Form.Control type = "date" onChange={evt => setExamDate(evt.target.value)} required />
+            </Form.Group>
+            <div>
+                {isLoading?
+                    <Spinner animation="border" role="status" variant = "dark" className = "mt-3">
+                        <span className="visually-hidden">Loading...</span>
+                    </Spinner>
+                    :
+                    <Button className = "ms-auto mt-3" variant = "dark" size = "lg" type= "submit">Vote</Button>
+                }
+            </div>
+        </Form>
+    )
 }
